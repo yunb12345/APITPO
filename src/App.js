8697285637d19bd4b3fbadc6e8514679a9f1d184
yunb12345 <agusyoon11@hotmail.com>
2024-08-31 18:26:35 -0300

    footer

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Navbar from './components/Navbar';
-
+import Hero from './components/Hero';
+import Footer from './components/Footer';
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Navbar/>
       <div className="container mx-auto px-3">
       </div>
+      <Footer/>
     </div>
 
   );
