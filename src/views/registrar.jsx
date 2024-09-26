deb40e890552eea8ffd103847113a29e4c7c327e
Mateo <matelacour@gmail.com>
2024-09-26 15:37:23 -0300

    Redirige al board al "registrarse"

--- src/views/registrar.jsx
+++ src/views/registrar.jsx
@@ -1,8 +1,34 @@
 import TextField from '@mui/material/TextField';
 import { Link } from "react-router-dom";
+import Modal from '@mui/material/Modal';
+import CustomBox from "../components/box";
+import { useNavigate } from "react-router-dom";
+import React, { useState } from "react";
 
 
 const Registrar =()=>{
+
+    const [nombre, setNombre] = useState("");
+    const [pass, setPass] = useState("");
+    const [email, setEmail] = useState("");
+
+    function validarMail(mail) {
+        const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        return regex.test(mail);
+    }
+
+    const navigate = useNavigate();
+    const handleRegister = () => {
+        if(nombre != "" && pass != "" && validarMail(email)){
+            navigate("/board");
+        } else {
+            console.log("error");
+            setOpenError(true);
+        }
+    };
+    const [openError, setOpenError] = React.useState(false);
+    const handleCloseError = () => setOpenError(false)
+
     return(
         <div className="bg-apigray h-screen overflow-x-hidden flex flex-wrap justify-center">
             <div className="mx-10 my-10 flex flex-wrap bg-slate-100">
@@ -32,24 +58,45 @@ const Registrar =()=>{
                             <h1 className='text-xl lg:text-4xl font-bold'>Crear Cuenta</h1>
                         </div>
                         <div className='flex flex-wrap py-5 gap-4 justify-center'>
-                            <TextField id="outlined-basic" label="Nombre" variant="outlined" size="small"/>
-                            <TextField id="outlined-basic" label="Email" variant="outlined" size="small"/>
+                            <TextField 
+                            id="outlined-basic" 
+                            label="Nombre" 
+                            variant="outlined" 
+                            size="small"
+                            value={nombre}
+                            onChange={(e) => setNombre(e.target.value)}
+                            />
+                            <TextField 
+                            id="outlined-basic" 
+                            label="Email" 
+                            variant="outlined" 
+                            size="small"
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                            />
                             <TextField
                                 id="outlined-password-input"
                                 label="Password"
                                 type="password"
                                 autoComplete="current-password"
                                 size="small"
+                                value={pass}
+                                onChange={(e) => setPass(e.target.value)}
                             />
                         </div>
                         <div className='py-5'>
-                            <button className="bg-apiyellow px-8 py-2 rounded-lg lg:px-20 hover:bg-apiyellowhover shadow-md transition-all">
+                            <button onClick={handleRegister} className="bg-apiyellow px-8 py-2 rounded-lg lg:px-20 hover:bg-apiyellowhover shadow-md transition-all">
                                 Registrar
                             </button>
                         </div>
                     </div>
                 </div>
             </div>
+            <Modal open={openError} onClose={handleCloseError}>
+                <CustomBox moreStyles={{width: 400 }}>
+                    <h2>Complete todos los campos con datos válidos</h2>
+                </CustomBox>
+            </Modal>
         </div>
     );
 }
